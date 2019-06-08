b39e9f7d7e434e8aeb74f98dbd0ef92091c3d1cb
tch180 <thopstadredner@gmail.com>
2019-06-08 13:22:24 -0400

    updated and added scss for homepage for text effect, installed node-sass, fixed navbar to top added a margin to admins to clear it away from navbar

--- src/admins.jsx
+++ src/admins.jsx
@@ -4,7 +4,7 @@ import admins1 from '../src/assets/images/admins.png'
 class admins extends Component {
     render() {
         return (
-            <div>
+            <div className="header mt-5"  >
                 <img src={admins1}  style={{display: 'block', marginLeft:'auto', marginRight:'auto', width:'95%'}}alt=""/>
             </div>
         );
--- src/home/Homepage.jsx
+++ src/home/Homepage.jsx
@@ -4,14 +4,23 @@ import allied from '../assets/images/Allied Shinobi Forces.png';
 import shadow from '../assets/images/destiny2.png';
 import shadow1 from '../assets/images/destiny3.jpg';
 
+import './homepage.scss';
+
 
 
 
 class Homepage extends Component {
+    
+
+   
+
+
+
 
     render() {
         return (
             <div>
+
                 <div style={{backgroundColor:"black" }}> 
                     <div className="Alliance-Name" style={{backgroundColor:"white",fontSize:"5vw", fontWeight:"bold", margin: "0 auto",  textAlign:"Center", position:"absolute", mixBlendMode:"screen", top:"25%",  }}>Allied Shinobi Forces</div>      
                     <img className="header  mx-5" src={shadow}  style={{width:'95%', backgroundColor:'black'}}alt=""/>
@@ -19,6 +28,16 @@ class Homepage extends Component {
 
                 <div className="parallax" style={{backgroundImage: `url(${shadow1})`, backgroundSize: "cover", backgroundAttachment:"fixed",height:"600px" }}>
                 <h1 style={{color:"white", textAlign:"Center",}}>"No matter how hard the mission, we will probably still die."</h1>
+                </div>
+
+                <div className="parallax2" style={{ backgroundColor:"black",height:"600px" }}> 
+
+                    <h1 className="clan-q" >
+                        When the  Red War began <span>they came to us.</span> <br/> When the Dreaming City fell under a curse <span>we were granted access to cleanse their city.</span> <br/> 
+                        They put all of their faith and trust into us, but where would they be without us? Imagine not having your light anymore. <span>A war is coming. </span> 
+                        This time <span> they will call upon us again. </span> The question, my fellow guardian, <span>will you answer?</span> 
+                    </h1>
+
                 </div>
                     
             </div>
