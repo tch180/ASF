83f4b095ac5fbcffb7ed75f966fb158a4fd31d57
tch180 <thopstadredner@gmail.com>
2019-06-20 02:00:03 -0400

    added video to streamer page, working on the layout, margins are a little off with the iframe. need to add 15px margin to top and bottom, bootstrap's mt-5 and mb-5 doesnt add enough to clear the fixed navbar, since this is a small fix will use inline style instead of a seperate css page

--- src/home/Homepage.jsx
+++ src/home/Homepage.jsx
@@ -33,8 +33,8 @@ class Homepage extends Component {
                 <div className="parallax2" style={{ backgroundColor:"black",height:"600px" }}> 
 
                     <h1 className="clan-q" >
-                        When the  Red War began <span>they came to us.</span> <br/> When the Dreaming City fell under a curse <span>we were granted access to cleanse their city.</span> <br/> 
-                        They put all of their faith and trust into us, but where would they be without us? Imagine not having your light anymore. <span>A war is coming. </span> 
+                        When the  Red War began <span>they came to us.</span> <br/> When the Dreaming City fell under a curse<br/> <span>we were granted access to cleanse their city.</span> <br/> 
+                        They put all of their faith and trust into us, <br/>but where would they be without us? Imagine not having your light anymore.<br></br> <span>A war is coming. </span> 
                         This time <span> they will call upon us again. </span> The question, my fellow guardian, <span>will you answer?</span> 
                     </h1>
 
--- src/streams.jsx
+++ src/streams.jsx
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
 
+
+
+
+
 class streams extends Component {
+
     render() {
+
+    
+
         return (
-                <div class = "card" style={{width: "18rem"}} > <img src="..." class="card-img-top" alt="..."/>
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make
-                             up the bulk of the card's content.</p>
-                 </div>
-                    <div class="card-body">
-                        {/* <a href="#" class="card-link">Card link</a>
+          <div>
+
+
+
+            <div className="jumbotron  jumbotron-fluid embed-responsive embed-responsive-21by9 mb-2 mt-5">
+              
+                  <iframe align='center' width="800" height="400" src="https://www.youtube.com/embed/xjVgKG465F8" frameBorder="1" 
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"  allowFullScreen>
+                  </iframe>
+              
+            </div>
+
+
+
+
+            <div className="card mt-5" style={{ width: "18rem" }}>
+              {" "}
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and
+                  make up the bulk of the card's content.
+                </p>
+              </div>
+              <div className="card-body">
+                {/* <a href="#" class="card-link">Card link</a>
                         <a href="#" class="card-link">Another link</a> */}
-                    </div>
-                </div> 
+              </div>
+            </div>
+
+
+          </div>
         );
     }
 }
